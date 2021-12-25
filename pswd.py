86cff0cdfb35cd2dbdfc080f3d57edaeb1d79843
ekow-lloyd <ekowlloyd@gmail.com>
2021-12-25 10:36:48 +0000

    added comments and fixed redundant code

--- pswd.py
+++ pswd.py
@@ -3,25 +3,34 @@ import pprint
 import pyinputplus as pyip
 from datetime import datetime, timedelta
 
-# creating the database
-
-""" with open("pswd_db.json", 'w') as writeFile:
-    json.dump(dataDb,writeFile) """
+# this block opens the dict in read mode to query it
 
 def new_record(credential,passwd):
         with open ("pswd_db.json", 'r') as read_file:
             pswd_records = json.load(read_file)
      
+ 
+ # created an empty dict, then assigned the variables that will be the key:value
+ # credential:passwd respectively
+
         data = {}    
         data[credential] = passwd
         pprint.pprint(data)
 
-        update = pswd_records.update(data)
+        
+# called the 'update' method to update original json dict that has been loaded into pswd_records variable
+# then open the dictionary in write mode this time to dump the newly data into it
+      
+        pswd_records.update(data)
         print('')
         print('Records updated')
 
         with open("pswd_db.json", "w") as write_file:
             json.dump(pswd_records, write_file)
+
+# using the while true helps keep the program running till terminated by user
+# dictionary is opened again in read mode in order to load the key:values into memory for query
+# user input is then required
 
 while True: 
 
@@ -32,9 +41,10 @@ while True:
     find_pwd = input("search for password of : ").lower().title()
     print('')
     
+
+# conditional block to tell if password already exist, if not further option to add    
     if find_pwd == '':
-        print("Nothing entered, exiting program")
-        print('')
+        print("Nothing entered, exiting program\n")
         quit()
 
     for k,v in pwdDb.items():   
@@ -43,8 +53,7 @@ while True:
     
     else:
         if find_pwd not in pwdDb:
-            print('Entry not found, would you like to save a new pswd? ')
-            print('')
+            print('Entry not found, would you like to save a new pswd?\n ')
 
             response = pyip.inputMenu(["Yes","No"],lettered=False, numbered=True)
             if response == "Yes":
